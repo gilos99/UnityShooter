59e3e899ec47498a9dafba4910037e99cd6778d5
Jan Gil <gilos1999@gmail.com>
2017-09-17 10:43:30 +0200

    First Commit / Scripts

---
+++ akScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class akScript : MonoBehaviour {
+
+	Transform me;
+	public Transform celownik;
+	public Transform positionAk;
+	public float machanie;
+	public bool machanieBool=true;
+	public float maxMachanie;
+	public float machanieAmount=0.02f;
+	void Start () {
+		me = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		poruszanie ();
+		me.position = new Vector3 (positionAk.position.x,positionAk.position.y+machanie,positionAk.position.z);
+		me.LookAt (new Vector3(celownik.position.x,celownik.position.y+machanie,celownik.position.z));
+
+	}
+	void poruszanie()
+	{
+		var up = Input.GetKey (KeyCode.W);
+		var down = Input.GetKey (KeyCode.S);
+		var left = Input.GetKey (KeyCode.A);
+		var right = Input.GetKey (KeyCode.D);
+		var shotUp = Input.GetMouseButtonUp (0);
+		var sprint = Input.GetKey (KeyCode.LeftShift);
+		if (machanie>maxMachanie) {
+			machanieBool = false;
+		}
+		else if (machanie<-maxMachanie) {
+			machanieBool = true;
+		}
+		if (machanieBool&&maxMachanie!=0) {
+			machanie += machanieAmount;
+		}
+		else if (!machanieBool&&maxMachanie!=0) {
+			machanie -= machanieAmount;
+		}
+		if (machanie>0&&maxMachanie==0) {
+			machanie -= machanieAmount;
+		}
+		if ((up||left||right)&&!sprint) {
+			maxMachanie = 0.05f;
+			machanieAmount = 0.007f;
+		}
+		else if ((up||left||right)&&sprint) {
+			maxMachanie = 0.07f;
+			machanieAmount = 0.01f;
+		}
+		else if (down) {
+			maxMachanie = 0.03f;
+			machanieAmount = 0.003f;
+		}
+		else if (!down&&!up&&!left&&!right) {
+			maxMachanie = 0f;
+		}
+		if (shotUp) {
+			machanie = 0.09f;
+
+		}
+
+	}
+}
---
+++ playerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+	public Camera MainCamera;
+	public Transform me;
+	public CharacterController playerCC;
+	public float aktualnaWysokosc=0f;
+	public float wysokoscSkoku=4f;
+	public float speed=9f;
+	public float czuloscMyszki;
+	float myszkaGoraDol;
+	float myszkaPrawoLewo;
+	 float przodTyl;
+	 float lewoPrawo;
+	public float predkoscBiegania = 7f;
+	
+	
+	// Use this for initialization
+	void Start () {
+		playerCC = GetComponent<CharacterController> ();
+		me = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouse ();
+		keyboard ();
+	}
+	void keyboard()
+	{
+		przodTyl = Input.GetAxis ("Vertical")*speed;
+		lewoPrawo = Input.GetAxis ("Horizontal") * speed;
+		if (playerCC.isGrounded&&Input.GetKey(KeyCode.Space)) {
+			aktualnaWysokosc = wysokoscSkoku;
+		}
+		else if (!playerCC.isGrounded) {
+			aktualnaWysokosc += Physics.gravity.y * Time.deltaTime;	
+		}
+
+		if (Input.GetKey(KeyCode.S)) 
+			{
+			predkoscBiegania=0;
+		}
+		else if (!Input.GetKey(KeyCode.S)) {
+				predkoscBiegania=7f;
+			}
+
+		if(Input.GetKeyDown("left shift")) {
+			speed+=predkoscBiegania;
+		} else if(Input.GetKeyUp("left shift")) {
+			speed-=predkoscBiegania;
+		}
+		Vector3 ruch = new Vector3 (lewoPrawo,aktualnaWysokosc,przodTyl);
+		ruch = transform.rotation * ruch;
+		playerCC.Move (ruch*Time.deltaTime);
+
+	}
+	void mouse()
+	{
+		myszkaPrawoLewo = Input.GetAxis ("Mouse X") * czuloscMyszki;
+		transform.Rotate (0,myszkaPrawoLewo,0);
+		myszkaGoraDol -= Input.GetAxis ("Mouse Y")*czuloscMyszki;
+		myszkaGoraDol = Mathf.Clamp (myszkaGoraDol,-90f,90f);
+		MainCamera.transform.localRotation = Quaternion.Euler (myszkaGoraDol,0,0);
+	}
+}
